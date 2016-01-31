d3b22febae17684fa25fd03640bc5f8fe5232990
JeffRisberg <jrisberg@aol.com>
2016-01-30 23:21:18 -0800

    started chapter 4 in the book (up to page 50)

--- app/router.js
+++ app/router.js
@@ -2,17 +2,19 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-Router.map(function() {
-  this.route('register');
-  this.route('login');
-  this.route('notebooks', { path: 'notebooks/:user_id'}, function() {
-    this.route('notes', { path: 'notes/:notebook_id'}, function() {});
-  });
-  this.route('dashboard');
-  this.route('configured');
+Router.map(function () {
+    this.route('register');
+    this.route('login');
+    this.route('notebooks', {path: 'notebooks/:user_id'}, function () {
+        this.route('notes', {path: 'notes/:notebook_id'}, function () {
+            this.route('note', {path: 'note/:note_id'}, function () {});
+        });
+    });
+    this.route('dashboard');
+    this.route('configured');
 });
 
 export default Router;
