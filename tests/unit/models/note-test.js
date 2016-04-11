4bf1a47a93798858ae8d24545ed1f6609b7b751f
JeffRisberg <jrisberg@aol.com>
2016-04-11 00:31:44 -0700

    Completed chapter 8 with all unit tests running

--- tests/unit/models/note-test.js
+++ tests/unit/models/note-test.js
@@ -2,11 +2,13 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('note', 'Unit | Model | note', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    'model:notebook'
+  ]
 });
 
 test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
+  var model = this.subject();
+  // var store = this.store();
   assert.ok(!!model);
 });
